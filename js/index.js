62e43fbfba5ef3243a1e463ea7909f44f33bc8df
brewchetta <chett.tiller@gmail.com>
2026-02-18 11:20:22 -0500

    move js to folder

--- js/index.js
+++ js/index.js
@@ -46,18 +46,3 @@ async function getAllAwardWinners() {
 
 // INITIAL CALL
 getAllAwardWinners()
-
-
-// RETURN AT 11:30 EST
-// and then... EXERCISES 
-
-// 1. Create a section for planetary buttons. Fetch the planet data and create a <button> for each of the planets in the json file.
-
-// 2. When a planet button is clicked, populate a section with all the information for that planet except the `is_planet` attribute. 
-// ...Use an <img> tag to show the image for that planet. 
-// ...Use a <ul> and populate the <ul> with <li>s for each string in the `god_of` section.
-
-// 3. Make sure to clear out the `god_of` section whenever a new planet is clicked and then repopulate that <ul>.
-
-// 4. If the planet is listed as `false` for `is_planet`, make the planet's <img> tag smaller (you can style it to have a smaller width) when setting the <img> tag's other attributes. 
-// If it's listed as `true` make the <img> tag larger (you can style it's width again).
