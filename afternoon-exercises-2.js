81d1080c8eb579d07f5d1dc9560fc5f5aad55c9e
brewchetta <chett.tiller@gmail.com>
2026-02-18 14:52:48 -0500

    raccoons exercise, clear out original exercise

---
+++ afternoon-exercises-2.js
@@ -0,0 +1,12 @@
+// Afternoon Exercises Part 2
+// const BASE_URL = "http://localhost:3000/raccoons"
+
+// 1. Copy the JSON data from GitHub into `raccoons.json` and start the server with `npx json-server raccoons.json`.
+
+// 2. When the page loads, make a fetch call to server for the raccoon data. Display each raccoon name as a button.
+
+// 3. Clicking the button fetches the details for that raccoon and displays the details in a container with their name, etc.
+
+// 4. If a raccoon does not have an image url, use a default stock raccoon image instead.
+
+// RETURN AT 3:22 EST
