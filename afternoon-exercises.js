943b57c09540b3d79fa37c65b1ca7779c9e2c253
brewchetta <chett.tiller@gmail.com>
2026-02-18 14:24:52 -0500

    add afternoon exercises

---
+++ afternoon-exercises.js
@@ -0,0 +1,12 @@
+// https://sv443.net/jokeapi/v2/#endpoints
+const BASE_URL = "https://v2.jokeapi.dev/joke/any"
+
+// 1. When the page loads, make a fetch call to the jokes API and display the setup for the joke. Include a button which when clicked will then display the punchline below the setup.
+
+// 2. Create a <button> which when clicked will fetch a brand new joke.
+
+// 3. Create a Like <button>. When a joke is "liked" it's setup and punchline are added as a <li> to a "Liked Jokes" <ul>.
+
+// 4. BONUS: Create a <select> for each of the different joke categories:
+// Any, Misc, Programming, Dark, Pun, Spooky, Christmas
+// The fetched joke will be that category. When the category changes, fetch a new joke.
